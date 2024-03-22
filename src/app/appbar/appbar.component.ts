51d933e744c40dc7ba916dcbee0c15cc07e4f160
Arrtorius <arrgagiev@gmail.com>
2024-03-23 01:51:11 +0300

    доработка appbar

--- src/app/appbar/appbar.component.ts
+++ src/app/appbar/appbar.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './appbar.component.scss'
 })
 export class AppbarComponent {
+  public menuItems = [
+    {title: 'Каталог', hasIcon: true},
+    {title: 'Доставка воды', hasIcon: true},
+    {title: 'О нас', hasIcon: false},
+    {title: 'О воде', hasIcon: false},
+    {title: 'Акции', hasIcon: false},
+    {title: 'Контакты', hasIcon: false},
+  ];
 
+  public user  = {
+    nane: 'Arthur',
+    age: 32
+  };
+
+  // это пример строчной конкатинации
+  // тут главное это (``[ё]) вот такие припизднутые кавычки
+  // и дальше выводится через $ как во flutter
+  public stroke = `ивет, выведи мне слово из строка ${this.user.nane} возрастом  ${this.user.age}`;
 }
